2a6d14614d6a6e317a7f0022825fc321c9dc23a0
Benedikt Albertsen <be.albertsen@gmail.com>
2021-08-09 15:57:26 +0200

    first

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,14 @@
 class QuestionsController < ApplicationController
   def ask
   end
+  def answer
+    @input = params[:question]
+    if @input == "I am going to work"
+      @input = "Great"
+    elsif @input[-1,1] == "?"
+      @input = "Silly question, get dressed and go to work!"
+    else
+      @input = "I don't care, get dressed and go to work!"
+    end
+  end
 end
